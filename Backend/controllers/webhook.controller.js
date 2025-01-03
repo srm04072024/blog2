91af4b62abcde4648825574453af9c6739742cbb
srm04072024 <s.r.m.20.3.2018@gmail.com>
2025-01-04 03:02:11 +0530

    Add delete user functionality

--- Backend/controllers/webhook.controller.js
+++ Backend/controllers/webhook.controller.js
@@ -6,29 +6,34 @@ export const clerkWebHook = async (req, res) => {
   if (!WEBHOOK_SECRET) {
     throw new Error("Webhook secret needed");
   }
-  // const headerPayload = req.headers;
-  // const svix_id = headerPayload["svix-id"];
-  // const svix_signature = headerPayload["svix-signature"];
-  // const svix_timestamp = headerPayload["svix-timestamp"];
+  const headerPayload = req.headers;
+  const svix_id = headerPayload["svix-id"];
+  const svix_signature = headerPayload["svix-signature"];
+  const svix_timestamp = headerPayload["svix-timestamp"];
   const payload = req.body;
-  const headers = req.headers;
+  // const headers = req.headers;
 
   const wh = new Webhook(WEBHOOK_SECRET);
   let evt;
   try {
-    evt = wh.verify(payload, headers);
+    evt = wh.verify(payload, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
+    });
   } catch (err) {
     console.log("Error", err);
-    return new Response("Error", { status: 400 });
+    res.status(400).json({
+      message: "Webhook verification failed!",
+    });
   }
-  console.log(evt.data);
 
   if (evt.type === "user.created") {
     const { email_addresses, primary_email_address_id } = evt.data;
     const userEmail = email_addresses.find(
       (email) => email.id === primary_email_address_id
     );
-    console.log(userEmail);
+
     const newUser = new User({
       clerkUserId: evt.data.id,
       username: evt.data.username || userEmail.email_address,
@@ -37,5 +42,17 @@ export const clerkWebHook = async (req, res) => {
     });
     await newUser.save();
   }
-  return new Response("Webhook Received", { status: 200 });
+
+  if (evt.type === "user.deleted") {
+    const deletedUser = await User.findOneAndDelete({
+      clerkUserId: evt.data.id,
+    });
+
+    await Post.deleteMany({ user: deletedUser._id });
+    await Comment.deleteMany({ user: deletedUser._id });
+  }
+
+  return res.status(200).json({
+    message: "Webhook received",
+  });
 };
