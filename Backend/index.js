d126e0e4a3ccd115606f6e7fe7f637f06de9f006
srm04072024 <s.r.m.20.3.2018@gmail.com>
2025-01-03 14:57:57 +0530

    Add CRUD Operation for Posts

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,9 @@ import userRouter from "./routes/user.route.js";
 import postRouter from "./routes/post.route.js";
 import commentRouter from "./routes/comment.route.js";
 import connectDB from "./lib/connectDB.js";
+
 const app = express();
+app.use(express.json());
 
 // app.get("/test", (req, res) => {
 //   res.status(200).send("It Works!!");
@@ -11,6 +13,14 @@ const app = express();
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    message: error.message || "Something Went Wrong!",
+    status: error.status,
+    stack: error.stack,
+  });
+});
 
 app.listen(4000, () => {
   connectDB();
